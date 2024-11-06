d79af8d39059f617b923777716a157f218787d75
Alexander Polanco <alexander.polanco@gmail.com>
2024-11-06 09:51:52 -0300

    add config for sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig, passthroughImageService } from "astro/config";
 import { loadEnv } from "vite";
 
+import sitemap from "@astrojs/sitemap";
+
 const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
+
   image: {
     service: passthroughImageService(),
   },
+
+  integrations: [sitemap()],
 });
